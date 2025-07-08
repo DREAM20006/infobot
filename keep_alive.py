837d99ba9a21371e855af716ea8dda55d42cb6af
Omkar Suryavanshi <152839635+DREAM20006@users.noreply.github.com>
2025-07-08 15:08:36 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,3 @@
-# keep_alive.py
 from flask import Flask
 from threading import Thread
 
@@ -6,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "🟢 I'm alive!"
+    return "✅ Bot running"
 
 def keep_alive():
     Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 8080}).start()
